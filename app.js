88bdc5c86d240132db972977c6fb386ee58bc95d
Karl-August Ko <kongopoiss@gmail.com>
2022-11-28 00:46:26 +0200

    Added loop functionality

---
+++ app.js
@@ -0,0 +1,38 @@
+class DrumKit {
+  constructor() {
+    this.pads = document.querySelectorAll(".pad");
+    this.kickAudio = document.querySelector(".kick-sound");
+    this.snareAudio = document.querySelector(".snare-sound");
+    this.hihatAudio = document.querySelector(".hihat-sound");
+    this.playBtn = document.querySelector(".play");
+    this.index = 0;
+    this.bpm = 150;
+  }
+  activePad() {
+    this.classList.toggle("active");
+  }
+  repeat() {
+    let step = this.index % 8;
+    const activeBars = document.querySelectorAll(`.b${step}`);
+    console.log(step);
+    this.index++;
+  }
+  start() {
+    const interval = (60 / this.bpm) * 1000;
+    //Use arrow function inside method's function
+    //cuz otherwise it will grab window not repeat()
+    setInterval(() => {
+      this.repeat();
+    }, interval);
+  }
+}
+
+const drumKit = new DrumKit();
+
+drumKit.pads.forEach((pad) => {
+  pad.addEventListener("click", drumKit.activePad);
+});
+
+drumKit.playBtn.addEventListener("click", () => {
+  drumKit.start();
+});
