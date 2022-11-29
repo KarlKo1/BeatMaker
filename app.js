991a2519c74ea1431742db2baee0e587f95958a7
Karl-August Ko <kongopoiss@gmail.com>
2022-11-30 00:00:06 +0200

    Improved functionality and added dark mode

--- app.js
+++ app.js
@@ -14,7 +14,26 @@ class DrumKit {
   repeat() {
     let step = this.index % 8;
     const activeBars = document.querySelectorAll(`.b${step}`);
-    console.log(step);
+    //Loop over the pads
+    activeBars.forEach((bar) => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+      //Check if pads are active
+      if (bar.classList.contains("active")) {
+        //Check each sound
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.currentTime = 0; //currentTime sets audio back to the start(0)
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      }
+    });
     this.index++;
   }
   start() {
@@ -31,8 +50,19 @@ const drumKit = new DrumKit();
 
 drumKit.pads.forEach((pad) => {
   pad.addEventListener("click", drumKit.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
 });
 
 drumKit.playBtn.addEventListener("click", () => {
   drumKit.start();
+});
+
+//Dark Mode
+let body = document.body;
+let darkMode = document.querySelector("#dark-change");
+darkMode.addEventListener("click", () => {
+  darkMode.classList.toggle("active");
+  body.classList.toggle("night");
 });
